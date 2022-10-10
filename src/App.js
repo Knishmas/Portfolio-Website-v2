8cd661675243f288ba48b33163a0b8f581dbda70
Andres <AndresEM1524@gmail.com>
2022-10-10 15:38:04 -0700

    assets added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useEffect, useState, useCallback, React } from 'react';
 import './App.css';
-
+import tv from './assets/tv.gif';
 function App() {
+
+  const descriptors = ['Dreamer', 'Creative explorer', 'Creator', 'Programmer']
+
+  const [descriptor,setDescriptor] = useState("Progammer");
+  const [showElement,setShowElement] = useState(false)
+
+  const randomDescriptor = useCallback(()=>{
+    const index = Math.floor(Math.random() * descriptors.length); 
+    setDescriptor(descriptors[index]);
+    setShowElement(false);
+  },[])
+
+    useEffect(() => {
+      let interval;
+          if(!showElement){ 
+             interval =  setInterval(() => {
+              setShowElement(true);
+            }, 500);
+          } else { 
+           interval = setInterval(randomDescriptor, 2500)
+          }
+          return () => {
+            clearInterval(interval);
+          }
+        }, [randomDescriptor, showElement])
+
+      
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+       <div className="banner">
+      <div className="title-content">
+        <h1>Hello Friend, <br />I'm Andres. </h1>
+        <h2>A programmer and a...</h2>
+       
+      </div>
+
+      <div className="tv-content">
+      {showElement && <div className="tv-overlay"> <h2 className='tv-text'>{descriptor}</h2> </div>}
+          <img class="television" src={tv} alt="TV" />
+         
+      </div>
+
+       </div>
+        
+
+      </body>
     </div>
+    
   );
 }
 
